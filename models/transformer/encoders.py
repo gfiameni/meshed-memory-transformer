a463b0d45daea5f6bf4195efcb0137a7708a6474
Zep <McK>
2020-04-14 15:49:58 +0200

    isolate transformer

--- models/transformer/encoders.py
+++ models/transformer/encoders.py
@@ -60,3 +60,39 @@ class MemoryAugmentedEncoder(MultiLevelEncoder):
         out = self.dropout(out)
         out = self.layer_norm(out)
         return super(MemoryAugmentedEncoder, self).forward(out, attention_weights=attention_weights)
+
+class BaseEncoder(nn.Module):
+    def __init__(self, N, padding_idx, d_model=512, d_k=64, d_v=64, h=8, d_ff=2048, dropout=.1,
+                 attention_module=None, attention_module_kwargs=None):
+        super(BaseEncoder, self).__init__()
+        self.d_model = d_model
+        self.dropout = dropout
+        self.layers = nn.ModuleList([EncoderLayer(d_model, d_k, d_v, h, d_ff, dropout,
+                                                  attention_module=attention_module,
+                                                  attention_module_kwargs=attention_module_kwargs)
+                                     for _ in range(N)])
+        self.padding_idx = padding_idx
+
+    def forward(self, input, attention_mask, attention_weights=None):
+        # input (b_s, seq_len, d_in)
+        out = input
+        for l in self.layers:
+            out = l(out, out, out, attention_mask, attention_weights)
+
+        return out, attention_mask
+
+class LinearEncoder(BaseEncoder):
+    def __init__(self, N, padding_idx, d_in=2048, **kwargs):
+        super(LinearEncoder, self).__init__(N, padding_idx, **kwargs)
+        self.fc = nn.Linear(d_in, self.d_model)
+        self.dropout = nn.Dropout(p=self.dropout)
+        self.layer_norm = nn.LayerNorm(self.d_model)
+
+    def forward(self, input, attention_weights=None):
+        attention_mask = (torch.sum(input, -1) == self.padding_idx).unsqueeze(1).unsqueeze(
+            1)  # (b_s, 1, 1, seq_len)
+
+        out = F.relu(self.fc(input))
+        out = self.dropout(out)
+        out = self.layer_norm(out)
+        return super(LinearEncoder, self).forward(out, attention_mask, attention_weights=attention_weights)
